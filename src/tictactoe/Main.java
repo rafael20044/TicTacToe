63773533e56e1d5de22c2af4d191ec340e0b6d98
Rafael <Rafaelenriquext@gmail.com>
2023-12-03 15:19:20 -0500

    El codigo del juego casi terminado solo faltaria agregar el modo jugador vs cpu

---
+++ src/tictactoe/Main.java
@@ -0,0 +1,15 @@
+package tictactoe;
+
+import tictactoe.ventanas.VentanaPrincipal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setVisible(true);
+        v.setTitle("Tic Tac Toe");
+        v.setLocationRelativeTo(null);
+                
+    }
+    
+}
